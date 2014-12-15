9ae63354b45f51afe6c89128a6b1639d4acfe29f
Martin Sch <amrastasu@gmail.com>
2014-12-15 10:54:15 -0200

    Primeiro commmit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppMarcadorPonto::Application.config.secret_token = '98ca05ddc7989a4f09a9b64ec14b40c0fc313dc89b4f03686e068276d0cadae6cf2bfe3412aa62f82cb0e50051a6cace7917046acac0cc623531dfe29594fde9'
